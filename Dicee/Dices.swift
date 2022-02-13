dd52b66b2257a29e97017ca2e6c45ef2b8ce7f5e
Flavius <flv.trc@outlook.com>
2022-02-13 20:58:40 +0200

    add outlets

--- Dicee/Dices.swift
+++ Dicee/Dices.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+struct Dices {
+    let diceOne: UIImage = #imageLiteral(resourceName: "DiceOne")
+    let diceTwo: UIImage = #imageLiteral(resourceName: "DiceTwo")
+    let diceThree: UIImage = #imageLiteral(resourceName: "DiceThree")
+    let diceFour: UIImage = #imageLiteral(resourceName: "DiceFour")
+    let diceFive: UIImage = #imageLiteral(resourceName: "DiceFive")
+    let diceSix: UIImage = #imageLiteral(resourceName: "DiceSix")
+}
--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    @IBAction func rollButton(_ sender: UIButton) {
+        printContent("Button is pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
